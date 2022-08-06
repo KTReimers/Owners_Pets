60e35197e35a2d6e69e9cdde42782abd1d5493a7
Kyle <reimers95@gmail.com>
2022-08-05 21:21:17 -0500

    Made a comment in the init

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import logging
+import logging #this is extra for deployment. to see traceback errors
 logging.basicConfig(filename='errorLog.log', level=logging.ERROR)
 app = Flask(__name__)
 app.secret_key = "shhhhhh"
